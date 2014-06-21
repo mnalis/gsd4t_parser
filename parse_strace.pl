bcc43db59dce114a2f15ee452afd53043ffb8ec0
Matija Nalis <mnalis-android@voyager.hr>
2014-06-21 15:03:07 +0200

    new method to parse to preserve timestamps

--- parse_strace.pl
+++ parse_strace.pl
@@ -8,7 +8,7 @@ use autodie;
 
 $| = 1;
 
-my $DEBUG = 9;
+my $DEBUG = 2;
 my $IN = 'data/strace3/strace.log.3382';
 open my $fd, '<', $IN;
 
@@ -17,51 +17,36 @@ open my $fd, '<', $IN;
 # | 00000  a0 a2 00 04 d2 01 09 00  4c 9d 72 e1 0a 14 23 08   ¢..Ò... L.rá..#. |
 
 
-my $log = '';
+my $last_hex = '';
+my $lasttime = undef;
 while (<$fd>) {
     print "raw: $_" if $DEBUG > 8;
-    next if (m{^(\d{2}:\d{2}:\d{2})(\.\d+) }); 	# FIXME: for now skip timestamp (and escaped read data, which we have in nicer hex format)
+    if (m{^(\d{2}:\d{2}:\d{2})(\.\d{3})\d* }) {
+        if (!defined($lasttime)) {
+          $lasttime = "$1$2";
+          print "Timestamp is now = $lasttime\n" if $DEBUG > 6;
+        }
+        next;
+    }
     next if (m{^\)});				# ignore end of syscall lines
     
     if (m{^\s+\|\s+[a-f0-9]+\s+(([a-f0-9]{2} ){1,8}( ([a-f0-9]{2} ){1,8}\s)?)}) {
       print "Start of hex $1\n" if $DEBUG > 5;
-      $log .= $1;
+      $last_hex .= $1;
+      if ($last_hex =~ /^(.*\s+b0\s+b3)(.*)$/i) {
+          $last_hex = $2;
+          my $packet = uc($1);
+          print "Found whole packet: $packet\n" if $DEBUG > 2;
+          
+          $packet =~ s/\s{2,}/ /g;
+          $packet =~ s/^\s+//;
+
+          # SLCLog.gp2 format is like:
+          # 21/06/2014 11:56:56.563 (0) A0 A2 00 0C FF 41 53 49 43 3A 20 47 53 44 34 54 03 DF B0 B3
+          print "00/00/0000 $lasttime (0) $packet\n";
+          $lasttime = undef;
+      }
     } else {
       die "unknown format for line: $_";
     }
-}
-
-close $fd;
-
-print "log = $log\n" if $DEBUG > 6;
-my @data = split ' ', $log;
-undef $log;
-
-
-# SLCLog.gp2 format is like:
-# 21/06/2014 11:56:56.563 (0) A0 A2 00 0C FF 41 53 49 43 3A 20 47 53 44 34 54 03 DF B0 B3
-
-my $seen_eol1 = 0;
-my $seen_eol2 = 1;
-while (my $x = shift @data) {
-  $x = uc($x);
-  if ($seen_eol2) {
-      $seen_eol2 = 0;
-      print "00/00/0000 00:00:00.000 (0) ";
-  }
-  
-  print "$x ";
-  if ($seen_eol1) {
-    $seen_eol1 = 0;
-    if ($x eq 'B3') {
-      print "\n";
-      $seen_eol2 = 1;
-    } else {
-      warn "invalid second terminator, expected B3 got $x" if $DEBUG > 2;
-    }
-  }
-  
-  if ($x eq 'B0') {
-    $seen_eol1 = 1;
-  }
 }
