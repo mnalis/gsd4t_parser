482b98aca34c838d9a96123e48d3dbcc35cf422e
Matija Nalis <mnalis-android@voyager.hr>
2014-06-25 13:00:41 +0200

    find double value

--- float.pl
+++ float.pl
@@ -1,5 +1,15 @@
+use feature 'say';
     my $num = 11.833496;
     
-        print join(" ", map { sprintf "0x%02x", $_ } 
+    print "num=$num";
+        print join(" ", map { sprintf "%02x", $_ } 
                             unpack("C*", pack "d", $num)), "\n";
                             
+                            
+my @hex = qw(40 27 AA C0 00 00 00 00);
+my @rev = reverse @hex;
+say "hex=@hex, rev=@rev";
+
+my $packed =  pack "H*", join('', @rev);
+#say "packed = $packed";
+say "decoded= " . unpack "d*", $packed;
