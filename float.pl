0716806e4a3b9b74e93ee06fd4c9c69bf6a80c02
Matija Nalis <mnalis-android@voyager.hr>
2014-07-07 17:33:01 +0200

    try permutations

--- float.pl
+++ float.pl
@@ -1,15 +1,27 @@
 use feature 'say';
-    my $num = 11.833496;
+    my $num = 43.724884;
     
-    print "num=$num";
-        print join(" ", map { sprintf "%02x", $_ } 
-                            unpack("C*", pack "d", $num)), "\n";
+    say "num=$num";
+    say join(" ", map { sprintf "%02x", $_ } 
+                            unpack("C*", pack "f", $num));
                             
                             
-my @hex = qw(40 27 AA C0 00 00 00 00);
+my @hex = qw(3E 2F BE 91);
 my @rev = reverse @hex;
 say "hex=@hex, rev=@rev";
 
 my $packed =  pack "H*", join('', @rev);
 #say "packed = $packed";
-say "decoded= " . unpack "d*", $packed;
+say "decoded= " . unpack "f*", $packed;
+
+
+say "trying all combinations";
+
+use Algorithm::Permute;
+
+Algorithm::Permute::permute {
+    my $packed =  pack "H*", join('', @hex);
+    my $float = unpack "f*", $packed;
+    my $double = unpack "d*", $packed;
+    say "  next permutation: @hex -- float=$float\tdouble=$double";
+} @hex;
