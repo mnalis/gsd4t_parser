e86b273acf707d8d10be8eb4da6e80683ae230cd
Matija Nalis <mnalis-android@voyager.hr>
2014-07-14 15:46:33 +0200

    comment (rare) b0b3 bug

--- sirfbin_to_gp2.pl
+++ sirfbin_to_gp2.pl
@@ -4,6 +4,7 @@
 # parses raw SiRF binary commands (like SLCLog.bin) and creates SLCLog.gp2 text file. 
 # Note: depends od A0A2 / B0B3, so only SiRF OSP (and perhaps enc4t) can be converted.
 
+# FIXME - sometimes it incorrectly splits into two packets when (B0 B3 found in payload). We could use packet size for raw binary SiRF OSP to fix that, but then this script could not process enc4t (it would need separate script). probably should be done, but can't bother right now, as it is easy to manually join a line or two in broken output
 #
 # Usage: ./sirfbin_to_gp2.pl < SLCLog.bin > SLCLog.gp2
 #
